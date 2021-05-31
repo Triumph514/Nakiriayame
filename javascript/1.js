da8ace20f1b190d3f1afffc3dc37f2a4123ad8e6
Triumph514 <82598152+Triumph514@users.noreply.github.com>
2021-05-31 22:18:37 +0800

    Add files via upload

---
+++ javascript/1.js
@@ -0,0 +1,15 @@
+window.onload = function(){
+    var ck = document.getElementById("ck");
+    var mask = document.getElementById("mask");
+    var definite = document.getElementById("definite");
+    var cc = document.getElementById("cc");
+        ck.onclick = function(){
+            mk.style.display = "block";
+        }
+        definite.onclick = function(){
+            mk.style.display = "none";
+        }
+        cc.onclick = function(){
+            mk.style.display = "none";
+        }
+}
